ee1e6c71091679f5edf50dbf2f1846116c92dfc1
Romina De León <romideleon@gmail.com>
2026-04-19 11:18:55 -0300

    Enhance Head component with Spanish support and metadata
    Updated the Head component to support Spanish language and added Open Graph and Schema.org metadata.

--- src/components/head.js
+++ src/components/head.js
@@ -4,11 +4,8 @@ import { Helmet } from 'react-helmet';
 import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
 
-// https://www.gatsbyjs.com/docs/add-seo-component/
-
 const Head = ({ title, description, image }) => {
   const { pathname } = useLocation();
-
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -24,36 +21,51 @@ const Head = ({ title, description, image }) => {
       }
     `,
   );
-
   const { defaultTitle, defaultDescription, siteUrl, defaultImage, twitterUsername } =
     site.siteMetadata;
-
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
     image: `${siteUrl}${image || defaultImage}`,
     url: `${siteUrl}${pathname}`,
   };
-
   return (
     <Helmet title={title} defaultTitle={seo.title} titleTemplate={`%s | ${defaultTitle}`}>
-      <html lang="en" />
-
+      {/* idioma a español */}
+      <html lang="es" />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta name="google-site-verification" content="9u5I3Ftz1BuzO1jce7bDkKJpNOtzuqW0mB0sbpJnrp8" />
 
-      {/* <meta property="og:title" content={seo.title} />
+      {/* Open Graph descomentado */}
+      <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
       <meta property="og:url" content={seo.url} />
-      <meta property="og:type" content="website" /> */}
+      <meta property="og:type" content="website" />
+      {/* locale en español */}
+      <meta property="og:locale" content="es_AR" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
+
+      {/* Schema.org  */}
+      <script type="application/ld+json">{`
+        {
+          "@context": "https://schema.org",
+          "@type": "Person",
+          "name": "Romina De León",
+          "url": "https://rominicky.github.io",
+          "image": "${seo.image}",
+          "sameAs": [
+            "https://orcid.org/0000-0003-2495-7213",
+            "https://github.com/rominicky"
+          ]
+        }
+      `}</script>
 
       <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <link rel="apple-touch-icon" href="/favicon.svg" type="image/svg+xml" />
